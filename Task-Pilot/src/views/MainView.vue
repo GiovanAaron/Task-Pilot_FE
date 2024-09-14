e19c6baab8a1c443ad0932d06dc17f16ad4b7cdb
Javan <Javan@Javans-MacBook-Pro.local>
2024-09-14 16:59:18 +0100

    sets up vue basic templates with typescript, testing tools and bootstrap

---
+++ Task-Pilot/src/views/MainView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SideBar from '@/components/SideBar.vue'
+</script>
+
+<template>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+</template>
