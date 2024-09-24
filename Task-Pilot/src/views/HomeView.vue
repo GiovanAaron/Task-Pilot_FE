0d695d5e02d21e9e1a590bbec893e57efd8ebac7
Javan <Javan@dhcp-10-100-123-236.guest.visitor>
2024-09-24 16:53:55 +0100

    creates status toggle button with custom colors, on click can access attributes to parent div

--- Task-Pilot/src/views/HomeView.vue
+++ Task-Pilot/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import StatusButton from '@/components/StatusButton.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <StatusButton />
   </main>
 </template>
